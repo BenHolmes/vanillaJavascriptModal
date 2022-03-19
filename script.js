ec804bfd85700ce49a250e9bab0e93d4998099f7
Benjamin Holmes <ben.holmes1357@gmail.com>
2022-03-18 19:47:11 -0700

    initial

---
+++ script.js
@@ -0,0 +1,9 @@
+const modalTrigger = document.querySelectorAll('.popupTrigger')
+const modalTriggerClose = document.querySelector('.popupModalClose')
+const greyedOutSite = document.querySelector('.greyedOut')
+
+modalTrigger.forEach( trigger => {
+    trigger.addEventListener('click', () => {
+        //console.log('hi')
+    })
+})
